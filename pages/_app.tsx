54c8b74261da82d34b3f17d6dc903de900878cd5
hughes <hughes@tumelo.com>
2020-04-29 19:06:56 +0100

    Added og:graph
    Signed-off-by: hughes <hughes@tumelo.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,7 +30,19 @@ class MyApp extends App {
             name="description"
             content="Hi, I’m Alex Hughes! I'm a Full-Stack Engineer and builder of high-growth startups based in Bristol. Speciality? Typically I build serverless web apps in React, GraphQL, and TypeScript."
           />
+          <meta
+            property="og:title"
+            content="React Engineer & Startup Founder | Alex Jack Hughes"
+          />
+          <meta
+            property="og:description"
+            content="Hi, I’m Alex Hughes! I'm a Full-Stack Engineer and builder of high-growth startups based in Bristol. Speciality? Typically I build serverless web apps in React, GraphQL, and TypeScript."
+          />
           <meta property="og:type" content="website" />
+          <meta
+            property="og:image"
+            content="https://alexhughes.io/template.png"
+          />
         </Head>
         <StaticKitProvider site="8f3036ece773">
           <Layout>
